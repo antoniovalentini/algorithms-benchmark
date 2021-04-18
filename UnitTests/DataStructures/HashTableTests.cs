4710c4b68e97cd6607d336cd703f3dce93a460a1
antoniovalentini <antonio.valentini@live.it>
2021-04-18 23:24:53 +0200

    hashtable draft

---
+++ UnitTests/DataStructures/HashTableTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodingChallengeCollection.DataStructures;
+using Xunit;
+
+namespace CodingChallengeCollection.UnitTests.DataStructures
+{
+    public class HashTableTests
+    {
+        [Theory]
+        [InlineData(1, "A")]
+        public void ShouldGetElement(int key, string value)
+        {
+            var table = new HashTable();
+            table.Put(key, value);
+            var actual = table.Get(1);
+            Assert.Equal(value, actual);
+        }
+    }
+}
---
+++ src/DataStructures/HashTable.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingChallengeCollection.DataStructures
+{
+    public class HashTable
+    {
+        public void Put(int key, string value)
+        {
+
+        }
+
+        public string Get(int key)
+        {
+            return null;
+        }
+
+        public void Remove(int key)
+        {
+
+        }
+    }
+}
