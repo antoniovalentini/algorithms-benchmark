7b7de1f2693031698064b30cbfdedd7df01c3bb1
antoniovalentini <antonio.valentini@live.it>
2024-11-29 15:41:04 +0100

    Merge Array

---
+++ UnitTests/Array/MergeArraysTests.cs
@@ -0,0 +1,45 @@
+﻿using CodingChallengeCollection.Algorithms.Arrays;
+using Xunit;
+
+namespace CodingChallengeCollection.UnitTests.Array;
+
+public class MergeArraysTests
+{
+    [Theory]
+    [MemberData(nameof(TestData), MemberType = typeof(MergeArraysTests))]
+    public void MergeArrays_Test(int[] nums1, int m, int[] nums2, int n, int[] expected)
+    {
+        MergeArrays.Merge(nums1, m, nums2, n);
+        Assert.Equal(expected, nums1);
+    }
+
+    public static TheoryData<int[], int, int[], int, int[]> TestData =>
+        new()
+        {
+            {
+                [1, 2, 3, 0, 0, 0], 3,
+                [2, 5, 6], 3,
+                [1, 2, 2, 3, 5, 6]
+            },
+            {
+                [1], 1,
+                [], 0,
+                [1]
+            },
+            {
+                [0], 0,
+                [1], 1,
+                [1]
+            },
+            {
+                [2,0], 1,
+                [1], 1,
+                [1, 2]
+            },
+            {
+                [-1,0,0,3,3,3,0,0,0], 6,
+                [1,2,2], 3,
+                [-1,0,0,1,2,2,3,3,3]
+            },
+        };
+}
---
+++ src/Algorithms/Arrays/MergeArrays.cs
@@ -0,0 +1,61 @@
+﻿namespace CodingChallengeCollection.Algorithms.Arrays;
+
+public class MergeArrays
+{
+    /* EASY
+     * You are given two integer arrays nums1 and nums2, sorted in non-decreasing order,
+     * and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+     * Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+     * The final sorted array should not be returned by the function, but instead be stored inside the array nums1.
+     * To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged,
+     * and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+     *
+     * Example 1:
+     *
+     * Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+     * Output: [1,2,2,3,5,6]
+     * Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
+     * The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
+     */
+    public static void Merge(int[] nums1, int m, int[] nums2, int n)
+    {
+        var cursor1 = 0;
+        var cursor2 = 0;
+        var temp = new int[m + n];
+
+        if (n == 0) return;
+
+        for (var i = 0; i < m+n; i++)
+        {
+            if (cursor1 >= nums1.Length)
+            {
+                temp[i] = nums2[cursor2];
+                cursor2++;
+                continue;
+            }
+
+            if (cursor2 >= nums2.Length)
+            {
+                temp[i] = nums1[cursor1];
+                cursor1++;
+                continue;
+            }
+
+            if (cursor1 < m && nums1[cursor1] <= nums2[cursor2])
+            {
+                temp[i] = nums1[cursor1];
+                cursor1++;
+            }
+            else
+            {
+                temp[i] = nums2[cursor2];
+                cursor2++;
+            }
+        }
+
+        for (var i = 0; i < m+n; i++)
+        {
+            nums1[i] = temp[i];
+        }
+    }
+}
