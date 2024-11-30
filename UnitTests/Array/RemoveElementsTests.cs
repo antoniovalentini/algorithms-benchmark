989b4817fc669c921aa8633494260bfe89e08ddc
antoniovalentini <antonio.valentini@live.it>
2024-11-30 14:52:47 +0100

    remove element

---
+++ UnitTests/Array/RemoveElementsTests.cs
@@ -0,0 +1,65 @@
+﻿using CodingChallengeCollection.Algorithms.Arrays;
+using Xunit;
+
+namespace CodingChallengeCollection.UnitTests.Array;
+
+public class RemoveElementsTests
+{
+    [Theory]
+    [MemberData(nameof(TestData1), MemberType = typeof(RemoveElementsTests))]
+    public void RemoveDuplicates_Test(int[] nums, int[] expected)
+    {
+        var actualK = RemoveElements.RemoveDuplicates(nums);
+
+        Assert.Equal(expected.Length, actualK);
+        for (var i = 0; i < actualK; i++) {
+            Assert.Equal(expected[i], nums[i]);
+        }
+    }
+
+    public static TheoryData<int[], int[]> TestData1 =>
+        new()
+        {
+            {
+                [1, 1, 2],
+                [1, 2]
+            },
+            {
+                [0, 0, 1, 1, 1, 2, 2, 3, 3, 4],
+                [0, 1, 2, 3, 4]
+            },
+            {
+                [1],
+                [1]
+            },
+        };
+
+    [Theory]
+    [MemberData(nameof(TestData2), MemberType = typeof(RemoveElementsTests))]
+    public void RemoveDuplicates2_Test(int[] nums, int[] expected)
+    {
+        var actualK = RemoveElements.RemoveDuplicates2(nums);
+
+        Assert.Equal(expected.Length, actualK);
+        for (var i = 0; i < actualK; i++) {
+            Assert.Equal(expected[i], nums[i]);
+        }
+    }
+
+    public static TheoryData<int[], int[]> TestData2 =>
+        new()
+        {
+            {
+                [1, 1, 1, 2, 2, 3],
+                [1, 1, 2, 2, 3]
+            },
+            {
+                [0, 0, 1, 1, 1, 1, 2, 3, 3],
+                [0, 0, 1, 1, 2, 3, 3]
+            },
+            {
+                [1],
+                [1]
+            },
+        };
+}
---
+++ src/Algorithms/Arrays/RemoveElement.cs
@@ -0,0 +1,62 @@
+﻿namespace CodingChallengeCollection.Algorithms.Arrays;
+
+public class RemoveElements
+{
+    public static int RemoveElement(int[] nums, int val) {
+        var k = 0;
+        var cursor1 = 0;
+        var cursor2 = nums.Length - 1;
+
+        if (nums.Length == 0) return 0;
+
+        while (cursor1 <= cursor2){
+            if (nums[cursor1] == val)
+            {
+                nums[cursor1] = nums[cursor2];
+                cursor2--;
+            }
+            else
+            {
+                cursor1++;
+                k++;
+            }
+        }
+        return k;
+    }
+
+    public static int RemoveDuplicates(int[] nums)
+    {
+        var n = nums.Length;
+        if (n <= 1) return n;
+
+        var j = 0;
+        for (var i = 0; i < n; i++)
+        {
+            if (j < 1 || nums[i] != nums[j - 1])
+            {
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+
+        return j;
+    }
+
+    public static int RemoveDuplicates2(int[] nums)
+    {
+        var n = nums.Length;
+        if (n <= 2) return n;
+
+        var j = 0;
+        for (var i = 0; i < n; i++)
+        {
+            if (j < 2 || nums[i] != nums[j - 2])
+            {
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+
+        return j;
+    }
+}
